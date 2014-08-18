104ba72866fed5f556007ff9b2c3d235c114581d
afrancoc2000 <afrancoc2000@gmail.com>
2014-08-17 19:00:53 -0500

    Se crea un controlador base para que controle la página inicial y la página de autenticación personalizada

---
+++ test/unit/com/sigi/BaseControllerSpec.groovy
@@ -0,0 +1,20 @@
+package com.sigi
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(BaseController)
+class BaseControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "Probar que la acción index retorna el modelo correcto"() {
+    }
+}
