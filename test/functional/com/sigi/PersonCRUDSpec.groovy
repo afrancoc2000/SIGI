bcdb618b0d9aaed9989b19eddffe9630201885cb
Ana Maria Franco <afrancoc2000@hotmail.com>
2014-08-20 12:32:50 -0500

    Agregue pruebas al login

--- test/functional/com/sigi/PersonCRUDSpec.groovy
+++ test/functional/com/sigi/PersonCRUDSpec.groovy
@@ -17,4 +17,22 @@ class PersonCRUDSpec extends GebReportingSpec {
        browser.page.title == "Login"
    }
    
+   def "equivocar login"() {
+	when:
+	j_username = "admin"
+	j_password = "password.malo"
+	submitInput.click()
+	then:
+	at "login/auth?login_error=1&format="
+   }
+   
+   def "realizar login"() {
+	when:
+	j_username = "admin"
+	j_password = "ana.franco.43159262"
+	submitInput.click()
+	then:
+	at "base/index"
+   }
+   
 }
