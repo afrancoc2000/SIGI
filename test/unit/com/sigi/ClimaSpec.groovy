7ed7337a728ff4f4ce7ad0ebc0ac7645a6e48e4a
afrancoc2000 <afrancoc2000@gmail.com>
2014-08-24 18:29:43 -0500

    Se crea el objeto de clima

---
+++ test/unit/com/sigi/ClimaSpec.groovy
@@ -0,0 +1,20 @@
+package com.sigi
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Clima)
+class ClimaSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
