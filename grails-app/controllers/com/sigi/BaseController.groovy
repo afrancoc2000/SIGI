3b5049185350577d3bf6e5548d4eb8030ad52de4
Ana Maria Franco <afrancoc2000@hotmail.com>
2014-08-27 10:50:40 -0500

    Update BaseController.groovy

--- grails-app/controllers/com/sigi/BaseController.groovy
+++ grails-app/controllers/com/sigi/BaseController.groovy
@@ -6,4 +6,27 @@ class BaseController {
 
 	@Secured(["hasAnyRole('ROL_USER', 'ROL_ADMIN')"])
     def index() { }
+    
+    def climas(){
+
+	def ubicacionJSON = request.JSON
+	def posActualX = ubicacionJSON.posX
+	def posActualY = ubicacionJSON.posY
+	
+    	def climas = Clima.findAllByPosXBetweenAndPosYBetween(posXActual - 10, posXActual + 10, posYActual - 10, posYActual + 10)
+
+	def climasJSON = []
+	climas.each { clima ->
+		def climaJSON = [:]
+		climaJSON['id'] = clima.id
+		climaJSON['posX'] = clima.posX
+		climaJSON['posY'] = clima.posY
+		climaJSON['tipo'] = clima.tipo
+		climaJSON['valor'] = clima.valor
+
+		climasJSON.add(climaJSON)
+	}
+	
+	render climasJSON as JSON
+    }
 }
